32904f81853b1301b67ec46559810b43a40ff95c
DevenMarrero <70239160+DevenMarrero@users.noreply.github.com>
2020-10-11 16:00:51 -0700

    Fixed impossible knobs

--- Knobs.py
+++ Knobs.py
@@ -2,11 +2,10 @@ from Voice import speak
 
 
 def knobs(posask):
-    answer = []
+    answer = 'not a knob'
 
     pos = posask.replace(' ', '').replace('three', '3').replace('six', '6').replace('five', '5').replace('two', '2')\
         .replace('to', '2').replace('too', '2').replace('one', '1').replace('-', '')
-
     if pos == '36' or pos == '35':
         answer = 'UP'
 
