4ce5949fe8baaf879ecafbb0b6eb6f6854c437bd
DevenMarrero <70239160+DevenMarrero@users.noreply.github.com>
2020-10-11 16:01:16 -0700

    Multiprocessing for OnFirst

--- Bot.py
+++ Bot.py
@@ -14,89 +14,131 @@ import KeyPad
 import SimpleWires
 import Simon
 
-while 1:
-    text = get_audio()
-    text = text.replace('fuse', 'diffuse').replace("Moore's", 'Morse').replace("/", '').lower()
+import multiprocessing
 
-    # Modules
-    if 'wires' in text:  # Name all wires. ie(red, blue, red)
-        text = text.replace('.', '').replace('wires ', '')
-        SimpleWires.simplewires(text)
 
-    elif 'button' in text:  # Colour then word ie(yellow Press)
-        text = text.replace('button ', '')
-        Button.button(text)
+def total_reset():
+    Morse.resetmorse()
+    WireSequence.resetsequence()
+    Morse.resetmorse()
+    Setup.resetsetup()
+    speak('Bomb Reset')
 
-    elif 'strip' in text:  # Color ie(Red)
-        text = text.replace('strip ', '')
-        Button.holdbutton(text)
 
-    elif 'keypad' in text or 'symbols' in text:  # Say symbols
-        text = text.replace('keypad ', '').replace('symbols ', '')
-        KeyPad.keypad(text)
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
 
-    elif 'simon' in text:  # Say colours ie(Red, Red, Blue)
-        text = text.replace('simon ', '')
-        Simon.simon(text)
+    while True:
+        txt = get_audio().lower()
+        text = txt.replace("moore's", 'morse').replace("/", ' ')
 
-    elif "words" in text or "on 1st" in text:  # Word on screen, then word on told button
-        text = text.replace("words ", '').replace("on first ", '')
-        OnFirst.onfirst(text)
+        # Modules
+        if 'wires' in text:  # Name all wires. ie(red, blue, red)
+            text = text.replace('.', '').replace('wires ', '')
+            SimpleWires.simplewires(text)
 
-    elif 'reset memory' in text:
-        Memory.resetmemstage()
-        speak('Memory reset')
+        elif 'button' in text:  # Colour then word ie(yellow Press)
+            text = text.replace('button ', '')
+            Button.button(text)
 
-    elif 'memory' in text:  # Number on screen
-        text = text.replace('for ', '4').replace('memory ', '')
-        Memory.memory(text)
+        elif 'strip' in text:  # Color ie(Red)
+            text = text.replace('strip ', '')
+            Button.holdbutton(text)
 
-    elif 'reset morse' in text:
-        Morse.resetmorse()
-        speak('Morse reset')
+        elif 'keypad' in text or 'symbols' in text:  # Say symbols
+            text = text.replace('keypad ', '').replace('symbols ', '')
+            KeyPad.keypad(text)
 
-    elif 'morse' in text:  # Sets of flashes ie(dot dot dash) or (0 0 1)
-        text = text.replace('morse ', '')
-        Morse.morse(text)
+        elif 'simon' in text:  # Say colours ie(Red, Red, Blue)
+            text = text.replace('simon ', '')
+            Simon.simon(text)
 
-    elif 'complicated' in text:  # Wire attributes separated by 'next' ie(red star next blue)
-        text = text.replace('complicated ', '').replace('read', 'red')
-        CompWires.compwires(text)
+        elif "words" in text or "on 1st" in text:  # Word on screen, then word on told button
+            text = text.replace("words ", '').replace("on 1st ", '')
+            if OnFirst.onfirst(text):
+                answer = OnFirst.onfirstwords()
+                p = multiprocessing.Process(target=speak, args=(answer,))
+                p.start()
 
-    elif 'sequence' in text or 'sequins' in text:  # colour then letter separated by 'next' ie(red charlie next blue bravo)
-        text = text.replace('sequins ', '').replace('sequence ', '')
-        WireSequence.sequence(text)
+                while p.is_alive():
+                    listen = get_audio()
+                    if listen == 'stop':
+                        p.terminate()
 
-    elif 'reset sequence' in text:
-        WireSequence.resetsequence()
-        speak('Sequence Reset')
+        elif 'reset memory' in text:
+            Memory.resetmemstage()
+            speak('Memory reset')
 
-    elif 'maze' in text or 'maize' in text or "may's" in text:  # 1 Indicator, start, finish sep by 'next' ie(44 next 43 next 55)
-        text = text.replace('maze ', '').replace('maize ', '').replace("may's ", '')
-        Maze.maze(text)
+        elif 'memory' in text:  # Number on screen
+            text = text.replace('for ', '4').replace('memory ', '')
+            Memory.memory(text)
 
-    elif 'password' in text:  # Nato alphabet for each letter sep by 'next' ie(lima next alpha)
-        text = text.replace('password ', '').replace('hilo', 'kilo')
-        Password.password(text)
+        elif 'reset morse' in text:
+            Morse.resetmorse()
+            speak('Morse reset')
 
-    elif 'knobs' in text:  # column of LEDs with top and bottom lit ie(135)
-        text = text.replace('knobs ', '')
-        Knobs.knobs(text)
+        elif 'morse' in text:  # Sets of flashes ie(dot dot dash) or (0 0 1)
+            text = text.replace('morse ', '')
+            Morse.morse(text)
 
-    # Commands
-    elif 'run setup' in text:
-        Setup.setup()
+        elif 'complicated' in text:  # Wire attributes separated by 'next' ie(red star next blue)
+            text = text.replace('complicated ', '').replace('read', 'red')
+            CompWires.compwires(text)
 
-    elif 'add strike' in text:
-        Setup.addstrike()
+        elif 'sequence' in text or 'sequins' in text:  # colour then letter sep by 'next' ie(red alpha next blue bravo)
+            text = text.replace('sequins ', '').replace('sequence ', '')
+            WireSequence.sequence(text)
 
-    elif 'remove strike' in text:
-        Setup.removestrike()
+        elif 'reset sequence' in text:
+            WireSequence.resetsequence()
+            speak('Sequence Reset')
 
-    elif 'the bomb is complete' in text:
-        speak('We did it')
-        break
+        elif 'maze' in text or 'maize' in text or "may's" in text:  # 1 Indicator, start, finish sep by 'next' ie(44 next 43 next 55)
+            text = text.replace('maze ', '').replace('maize ', '').replace("may's ", '')
+            Maze.maze(text)
 
-    elif 'we blew up' in text:
-        speak("It's Not my Fault")
-        break
+        elif 'password' in text:  # Nato alphabet for each letter sep by 'next' ie(lima next alpha)
+            text = text.replace('password ', '').replace('hilo', 'kilo')
+            Password.password(text)
+
+        elif 'knobs' in text:  # column of LEDs with top and bottom lit ie(135)
+            text = text.replace('knobs ', '')
+            Knobs.knobs(text)
+
+        # Setup
+        elif 'batteries' in text:
+            text = text.replace('batteries ', '')
+            Setup.batteries(text)
+
+        elif 'serial' in text or 'cereal' in text:
+            text = text.replace('serial ', '')
+            Setup.serial(text)
+
+        elif 'port' in text or 'parallel port' in text:
+            text = text.replace('port ', '').replace('parallel port ', '')
+            Setup.port()
+
+        elif 'indicators' in text:
+            text = text.replace('indicators ', '')
+            Setup.indicators(text)
+
+        # Commands
+        elif 'add strike' in text:
+            Setup.addstrike()
+
+        elif 'remove strike' in text:
+            Setup.removestrike()
+
+        elif text == 'the bomb is complete':
+            speak('We did it')
+            total_reset()
+
+        elif 'we blew up' in text:
+            speak("It's Not my Fault")
+            total_reset()
+
+        elif 'reset bomb' in text or 'total reset' in text:
+            total_reset()
+
+        elif 'exit program' in text or 'end program' in text:
+            break
